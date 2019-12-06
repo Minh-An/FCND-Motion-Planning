c2cbad8de11f1b727c29982c1a60ff7dc4307dbd
An Doan <minh.an.doan@gmail.com>
2019-12-05 21:56:25 -0800

    Add diagonal movements to the grid and a prune path method.

--- planning_utils.py
+++ planning_utils.py
@@ -55,6 +55,10 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
+    NORTHEAST = (1,1, np.sqrt(2))
+    NORTHWEST = (1,-1, np.sqrt(2))
+    SOUTHEAST = (-1,1, np.sqrt(2))
+    SOUTHWEST = (-1,-1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -63,7 +67,6 @@ class Action(Enum):
     @property
     def delta(self):
         return (self.value[0], self.value[1])
-
 
 def valid_actions(grid, current_node):
     """
@@ -78,12 +81,20 @@ def valid_actions(grid, current_node):
 
     if x - 1 < 0 or grid[x - 1, y] == 1:
         valid_actions.remove(Action.NORTH)
+        valid_actions.remove(Action.NORTHEAST)
+        valid_actions.remove(Action.NORTHWEST)
     if x + 1 > n or grid[x + 1, y] == 1:
         valid_actions.remove(Action.SOUTH)
+        valid_actions.remove(Action.SOUTHEAST)
+        valid_actions.remove(Action.SOUTHWEST)
     if y - 1 < 0 or grid[x, y - 1] == 1:
         valid_actions.remove(Action.WEST)
+        if Action.NORTHWEST in valid_actions: valid_actions.remove(Action.NORTHWEST)
+        if Action.SOUTHWEST in valid_actions: valid_actions.remove(Action.SOUTHWEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
+        if Action.NORTHEAST in valid_actions: valid_actions.remove(Action.NORTHEAST)
+        if Action.SOUTHEAST in valid_actions: valid_actions.remove(Action.SOUTHEAST)
 
     return valid_actions
 
@@ -144,3 +155,25 @@ def a_star(grid, h, start, goal):
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
 
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6):
+    m = np.concatenate((p1, p2, p3), 0)
+    det = np.linalg.det(m)
+    return abs(det) < epsilon
+
+def prune_path(path):
+    pruned_path = [p for p in path]
+    
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
+        
+        if collinearity_check(p1, p2, p3):
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+    return pruned_path
